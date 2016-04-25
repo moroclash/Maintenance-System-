7f65ba079f1674b59dccd5b1b72649a592c41061
mohamed salah <mohamed salah>
2016-04-25 14:14:31 -0700

    sala7

--- src/java/Data_access/DB_controller.java
+++ src/java/Data_access/DB_controller.java
@@ -6,6 +6,7 @@
 package Data_access;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -29,11 +30,24 @@ public class DB_controller {
     
     public static void Connect ()
     {
-        
+       try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            DB_controller = DriverManager.getConnection("jdbc:mysql://localhost:3306/maintenenceWEB?zeroDateTimeBehavior=convertToNull","root", "");
+            System.out.println("ConnectionDB Done");
+        }
+        catch(Exception x)
+        {
+            System.out.println("Eroro LL2sf fe el Coonection");
+        } 
     }
     public static void Close ()
     {
-        
+       try {
+            DB_controller.close();
+            System.out.println("CloseDB Done");
+        } catch (SQLException ex) {
+            System.out.println("Eror in function close DB");
+        }  
     }
     public static ResultSet Select (String FieldName , String TableName , String Condetion)
     {
@@ -53,7 +67,14 @@ public class DB_controller {
 }
     public static boolean Update (String TableName , String FieldName , String Condition)
     {
-        return true;
+        String Query = " UPDATE " + TableName + " set " + FieldName + " WHERE " + Condition;
+       try {
+              PreparedStatement pre = DB_controller.prepareStatement(Query); 
+              return true;
+        } catch (SQLException ex){
+            ex.getStackTrace();
+            return false;
+        }
     }
     public static boolean Insert(String TableName , HashMap<String,String> values)
     {
