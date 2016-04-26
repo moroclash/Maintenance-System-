1dd517d6242bcd58145e1862efd1ccd4c9ab547d
Emad-Sayed <emad_tttt522@hotmail.com>
2016-04-26 14:37:26 +0200

    3wel2

--- src/java/Data_access/DB_controller.java
+++ src/java/Data_access/DB_controller.java
@@ -11,66 +11,94 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author Mohamed Salah
  */
 public class DB_controller {
-    
+
     private static Connection DB_controller;
 
-    public static void Connect ()
-    {
-       try {
+    public static void Connect() {
+        try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            DB_controller = DriverManager.getConnection("jdbc:mysql://localhost:3306/maintenenceWEB?zeroDateTimeBehavior=convertToNull","root", "");
+            DB_controller = DriverManager.getConnection("jdbc:mysql://localhost:3306/maintenenceWEB?zeroDateTimeBehavior=convertToNull", "root", "");
             System.out.println("ConnectionDB Done");
-        }
-        catch(Exception x)
-        {
+        } catch (Exception x) {
             System.out.println("Eroro LL2sf fe el Coonection");
-        } 
+        }
     }
-    public static void Close ()
-    {
-       try {
+
+    public static void Close() {
+        try {
             DB_controller.close();
             System.out.println("CloseDB Done");
         } catch (SQLException ex) {
             System.out.println("Eror in function close DB");
-        }  
+        }
     }
-    public static ResultSet Select (String FieldName , String TableName , String Condetion)
-    {
-      return null;  
+
+    public static ResultSet Select(String FieldName, String TableName, String Condetion) {
+        return null;
     }
-    public static boolean Delete (String TableName , String condition)
-{
-  String  Query="DELETE FROM "+TableName+" WHERE "+condition;  
-      try {
-              PreparedStatement pre = DB_controller.prepareStatement(Query); 
-              pre.execute();
-              return true;
-          } catch (SQLException ex){
+
+    public static boolean Delete(String TableName, String condition) {
+        String Query = "DELETE FROM " + TableName + " WHERE " + condition;
+        try {
+            PreparedStatement pre = DB_controller.prepareStatement(Query);
+            pre.execute();
+            return true;
+        } catch (SQLException ex) {
             ex.getStackTrace();
             return false;
         }
-}
-    public static boolean Update (String TableName , String FieldName , String Condition)
-    {
+    }
+
+    public static boolean Update(String TableName, String FieldName, String Condition) {
         String Query = " UPDATE " + TableName + " set " + FieldName + " WHERE " + Condition;
-       try {
-              PreparedStatement pre = DB_controller.prepareStatement(Query); 
-              return true;
-        } catch (SQLException ex){
+        try {
+            PreparedStatement pre = DB_controller.prepareStatement(Query);
+            return true;
+        } catch (SQLException ex) {
             ex.getStackTrace();
             return false;
         }
     }
-    public static boolean Insert(String TableName , HashMap<String,String> values)
-    {
-        return true;
+
+    //Emad
+    public static boolean Insert(String TableName, HashMap<String, String> values) {
+        String key = "";
+        String value = "";
+        String k;
+        String v;
+        for (Map.Entry<String, String> entry : values.entrySet()) {
+            k = entry.getKey();
+            v = entry.getValue();
+            key += k + ",";
+            value += v + ",";
+        }
+        String Attributes = "";
+        String Values = "";
+        for (int i = 0; i < key.length() - 1; i++) {
+            Attributes += key.charAt(i);
+        }
+        for (int i = 0; i < value.length() - 1; i++) {
+            Values += value.charAt(i);
+        }
+        Attributes = "(" + Attributes + ")";
+        Values = "(" + Values + ")";
+        String Query = "INSERT INTO " + TableName + " " + Attributes + " VALUES '" + Values + "' ";
+        try {
+            PreparedStatement pre = DB_controller.prepareStatement(Query);
+            pre.execute();
+            return true;
+        } catch (SQLException ex)
+        {
+            System.out.println("Error in Insert Function");
+            return false;
+        }
     }
-    
+
 }
